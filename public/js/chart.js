a659f1e84f3984dbc86265db94d3b82ba16bb8f8
Vikram Bala <vikrambala2002@gmail.com>
2022-05-22 22:40:10 -0400

    Parse input from web fields

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,9 +1,9 @@
 //List of chords entered by the user
-var chordList = document.getElementsByTagName("LI"); //Live List!
+var chordList = document.getElementsByTagName("li"); //Live List!
 var deleteButtons = document.getElementsByClassName("delete"); ///Live list!
 
 //Creates delete button for each chord
-let generateDeleteButtons = () => {
+var generateDeleteButtons = () => {
     for (var i = 0; i < chordList.length; i++) {
         //If the list element does not already have a delete button then add one
         if (chordList[i].getElementsByClassName("delete-button").length === 0) {
@@ -19,13 +19,42 @@ let generateDeleteButtons = () => {
     }
 }
 
+//Generate chart from chords inputted.
+function generateChartParams() {
+    let sections = ['A']; //Solely use one A section in compositions
+    let chord_duration_pairs = []; //2D list, outer list contains sections, inner lists are the chords in each section. 
+    //For each entered chord/duration add it to the chord_duration_pairs list.
+    for (var s = 0; s < sections.length; s++) {
+        let section_chord_duration_pairs = [];
+        for (var i = 0; i < chordList.length; i++) {
+            let li = chordList[i];
+            let chordString = li.classList[0];
+            let durationString = li.classList[1];
+            let chord_duration_pair = {chord: chordString, duration: parseInt(durationString)};
+            section_chord_duration_pairs.push(chord_duration_pair);
+        }
+        chord_duration_pairs.push(section_chord_duration_pairs);
+    }
+    
+
+    var section_chord_map = {};
+    //Assign a key with the section name to the associated chord/duration pairs list.
+    sections.forEach((section, index) => section_chord_map[section] = chord_duration_pairs[index]);
+    console.log(sections.toString());
+    console.log(JSON.stringify(section_chord_map).toString());
+    //document.getElementById("composition-name").value
+
+
+}
 
 //Add a new chord to the chart
-let addChord = () => {
+var addChord = () => {
     var li = document.createElement("li");
     var chordString = document.getElementById("chord").value;
     var duration = document.getElementById("duration").value;
     var txt = document.createTextNode("Chord: " + chordString + ",           Duration: " + duration + " measures ")
+    li.classList.add(chordString); 
+    li.classList.add(duration);
     li.style = "margin-bottom: 3px"
     li.appendChild(txt);
 
--- routes/improv.js
+++ routes/improv.js
@@ -4,18 +4,33 @@
  * Github Repository: https://github.com/vbala29/Jazz-Improv-Bot#readme
  */
 
- const express = require('express')
- const router = express.Router();
- const locals = require('../app.locals.js')
- const User = require(locals.models + '/user');
- const passport = require('passport');
- const asyncHandler = require(locals.scripts + '/asyncHandler.js')
- const isLoggedIn = require(locals.scripts + '/isLoggedIn')
+const express = require('express')
+const router = express.Router();
+const locals = require('../app.locals.js')
+const User = require(locals.models + '/user');
+const passport = require('passport');
+const asyncHandler = require(locals.scripts + '/asyncHandler.js')
+const isLoggedIn = require(locals.scripts + '/isLoggedIn')
+const s11 = require('sharp11')
+
+function generateChartObject(sections, section_chord_map, info) {
+   //     var chart = chart.create(['A'], {A : [{chord: 'C7', duration: 4}]}, {foo: 1});
+   var chart = chart.create(sections, section_chord_map, info); 
+}
 
 
- router.get('/improv', isLoggedIn, (req, res) => {
-    res.render('improv/improv.ejs', {error: req.flash("error"), success: req.flash("success")})
- })
+router.get('/improv', (req, res) => {
+   res.render('improv/improv.ejs', {error: req.flash("error"), success: req.flash("success")})
+})
 
 
- module.exports = router;
+router.get('/generateChart', isLoggedIn, (req, res) => {
+   
+})
+
+//  router.get('/improv', isLoggedIn, (req, res) => {
+//     res.render('improv/improv.ejs', {error: req.flash("error"), success: req.flash("success")})
+//  })
+
+
+module.exports = router;
