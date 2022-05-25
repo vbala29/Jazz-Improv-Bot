412d3e8d53aa1ac42827d67b11d7e4365aa80df9
Vikram Bala <vikrambala2002@gmail.com>
2022-05-25 01:28:55 -0400

    Prepared for AWS Lambda to be setup, cleaned up requests to backend for selection

--- public/js/selectChart.js
+++ public/js/selectChart.js
@@ -1,33 +1,20 @@
-
 //List of chords entered by the user
-var chordList = document.getElementsByTagName("li"); //Live List!
-var selectButtons = document.getElementsByClassName("select"); ///Live list!
+var chartList = document.getElementsByClassName("select-button"); //Live List!
 
-//Creates delete button for each chord
-var generateSelectButtons = () => {
-    for (var i = 0; i < chordList.length; i++) {
-        //If the list element does not already have a delete button then add one
-        if (chordList[i].getElementsByClassName("select-button").length === 0) {
-            var button = document.createElement("button");
-            button.className = "delete-button btn btn-secondary"
-            button.innerHTML = "Select Chart"
-            button.name = "Select-Chart"
-            // button.onclick
-            chordList[i].appendChild(button)
-        }
+var generateCallbacks = () => {
+    for (var i = 0; i < chartList.length; i++) {
+        chartList[i].addEventListener('click', function() {
+            fetch('http://localhost:3000/improv/improviseOnChart', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+            }).then(
+                console.log("Here! " + this.id)
+            ).catch(err => alert("Server Error: " + err));
+        });
     }
 }
-
-async function selectChart() {
-    fetch('http://localhost:3000/improv/improviseOnChart', {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-    }).then(
-        
-    ).catch(err => alert("Server Error: " + err));
-};
 
 
